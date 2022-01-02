6485e65e3b1490c889646f6b7a55ce16d1383b1f
igor <gsambir519@gmail.com>
2022-01-02 22:53:26 +0200

    new windows

--- kinoteatr/settings.py
+++ kinoteatr/settings.py
@@ -142,3 +142,9 @@ AUTH_USER_MODEL = 'users.User'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+# EMAIL_HOST='smtp.gmail.com'
+# EMAIL_PORT=587
+# EMAIL_HOST_USER = 'gsambir519@gmail.com'
+# EMAIL_HOST_PASSWORD = 'tytpassword123!i'
+# DEFAULT_FROM_EMAIL = 'gsambir519@gmail.com'
+# EMAIL_USE_TLS = True
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from django.urls import path, include
 from . import views
 from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView, SetPasswordForm
